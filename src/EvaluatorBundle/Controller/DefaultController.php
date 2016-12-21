595d0c2fe10ac18345481e8b97086bd412f3fb1d
Rafael Santamaria <kuithsteam@gmail.com>
2016-12-21 22:56:03 +0100

    intentando OneToMany en course

--- src/EvaluatorBundle/Controller/DefaultController.php
+++ src/EvaluatorBundle/Controller/DefaultController.php
@@ -8,30 +8,6 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-//		$em = $this->getDoctrine()->getManager();
-//		$student_repo = $em->getRepository("EvaluatorBundle:Student");
-//		$students = $student_repo->findAll();
-//		
-//		foreach ($students as $student){
-//			echo $student->getName(). "<br/>";
-//			echo $student->getIdCourse()->getName()."<br/> <hr/>";
-//		}
-	
-		$em = $this->getDoctrine()->getManager();
-		$course_repo = $em->getRepository("EvaluatorBundle:Course");
-		$courses = $course_repo->findAll();
-		
-		foreach ($courses as $course){
-			//echo $course->getName(). "<br/>";
-			$students = $course->getStudent();
-			
-			foreach ($students as $student){
-				echo $student->getName(). "<br/>";
-			}
-		}
-		
-		die();
-		
         return $this->render('EvaluatorBundle:Default:index.html.twig');
     }
 }
