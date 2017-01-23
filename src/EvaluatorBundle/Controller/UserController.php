551690550ca5cedf2772d951aa53b31330feba55
Rafael Santamaria <kuithsteam@gmail.com>
2017-01-23 23:11:09 +0100

    formulario de registro - 1

--- src/EvaluatorBundle/Controller/UserController.php
+++ src/EvaluatorBundle/Controller/UserController.php
@@ -4,6 +4,9 @@ namespace EvaluatorBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use EvaluatorBundle\Entity\User;
+use EvaluatorBundle\Form\UserType;
+
 
 class UserController extends Controller
 {
@@ -13,9 +16,21 @@ class UserController extends Controller
 		$error = $authenticationUtils->getLastAuthenticationError();
 		$lastUserName = $authenticationUtils->getLastUsername();
 		
+		$user = new User();
+		$form = $this->createForm(UserType::class, $user);
+		$form->handleRequest($request);
+		
+		if($form->isValid()){
+			$status = "El usurio se ha creado correctamente";
+		} else {
+			$status = "No te has registrado correctamente";
+		}
+		
+		
         return $this->render('EvaluatorBundle:User:login.html.twig', array(
 			"error" => $error,
-			"last_username" => $lastUserName
+			"last_username" => $lastUserName,
+			"form" => $form->createView()
 		));
     }
 }
