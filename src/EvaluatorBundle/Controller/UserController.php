65ca5175376f63d30e8e99e743be73f5fee14a2f
Rafael Santamaria <kuithsteam@gmail.com>
2017-01-26 23:29:31 +0100

    formulario de registro - 2

--- src/EvaluatorBundle/Controller/UserController.php
+++ src/EvaluatorBundle/Controller/UserController.php
@@ -7,30 +7,40 @@ use Symfony\Component\HttpFoundation\Request;
 use EvaluatorBundle\Entity\User;
 use EvaluatorBundle\Form\UserType;
 
+class UserController extends Controller {
 
-class UserController extends Controller
-{
-    public function loginAction(Request $request)
-    {
+	public function loginAction(Request $request) {
 		$authenticationUtils = $this->get("security.authentication_utils");
 		$error = $authenticationUtils->getLastAuthenticationError();
 		$lastUserName = $authenticationUtils->getLastUsername();
-		
+
 		$user = new User();
 		$form = $this->createForm(UserType::class, $user);
 		$form->handleRequest($request);
-		
-		if($form->isValid()){
-			$status = "El usurio se ha creado correctamente";
+
+		if ($form->isValid()) {
+			$user = new User();
+			$user->setName($form->get("name")->getData());
+			$user->setPassword($form->get("password")->getData());
+
+			$em = $this->getDoctrine()->getManager();
+			$em->persist($user);
+			$flush = $em->flush();
+			if ($flush == null) {
+				$status = "El usurio se ha creado correctamente";
+			} else {
+				$status = "No te has registrado correctamente";
+			}
 		} else {
 			$status = "No te has registrado correctamente";
 		}
-		
-		
-        return $this->render('EvaluatorBundle:User:login.html.twig', array(
-			"error" => $error,
-			"last_username" => $lastUserName,
-			"form" => $form->createView()
+
+
+		return $this->render('EvaluatorBundle:User:login.html.twig', array(
+					"error" => $error,
+					"last_username" => $lastUserName,
+					"form" => $form->createView()
 		));
-    }
+	}
+
 }
--- src/EvaluatorBundle/Form/UserType.php
+++ src/EvaluatorBundle/Form/UserType.php
@@ -5,6 +5,9 @@ namespace EvaluatorBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class UserType extends AbstractType
 {
@@ -14,8 +17,15 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-			->add('name')
-			->add('password')
+			->add('name', TextType::class, array("label"=>"Nombre", "required"=>"required", "attr"=>array(
+				"class"=>"form-name form-control",
+			)))
+			->add('password', PasswordType::class, array("label"=>"Contraseña", "required"=>"required", "attr"=>array(
+				"class"=>"form-password form-control",
+			)))
+			->add('guardar', SubmitType::class, array("attr"=>array(
+				"class"=>"form-submit btn btn-success",
+			)))
 		;
     }
     
