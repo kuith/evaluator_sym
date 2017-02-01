6e381e0e47169de7170450cfd39793c33408f109
Rafael Santamaria <kuithsteam@gmail.com>
2017-02-01 22:51:44 +0100

    Formualrio de creacion de cursos - 2

--- src/EvaluatorBundle/Controller/CourseController.php
+++ src/EvaluatorBundle/Controller/CourseController.php
@@ -9,21 +9,37 @@ use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\HttpFoundation\Request;
 
 class CourseController extends Controller {
-	
+
 	private $session;
+
 	public function __construct() {
 		$this->session = new Session();
 	}
 
-public function addAction(Request $request){
-	
-	$course = new Course();
-	$form = $this->createForm(CourseType::class, $course);
-	
-	return $this->render("EvaluatorBundle:Course:add.html.twig", array(
-		"form" => $form->createView()
-	));
-}
-	
+	public function addAction(Request $request) {
+
+		$course = new Course();
+		$form = $this->createForm(CourseType::class, $course);
+
+		$form->handleRequest($request);
+
+		if ($form->isSubmitted()) {
+			if ($form->isValid()) {
+//			aqui irá la lógica pra dar de alta el nuevo curso
+				$status = "El curso se ha dado de alta correctamente.";
+			} else {
+				$status = "El curso no se ha podido dar de alta.";
+			}
+			$this->session->getFlashBag()->add("status", $status);
+		}
+
+
+
+
+
+		return $this->render("EvaluatorBundle:Course:add.html.twig", array(
+					"form" => $form->createView()
+		));
+	}
 
 }
