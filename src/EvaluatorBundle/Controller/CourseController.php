85969e4d320926cc4e78f0d5000911aa8e0966a9
Rafael Santamaria <kuithsteam@gmail.com>
2017-02-02 22:58:32 +0100

    Formualrio de creacion de cursos - 3

--- src/EvaluatorBundle/Controller/CourseController.php
+++ src/EvaluatorBundle/Controller/CourseController.php
@@ -16,6 +16,17 @@ class CourseController extends Controller {
 		$this->session = new Session();
 	}
 
+	public function indexAction(){
+		$em = $this->getDoctrine()->getManager();
+		$course_repo = $em->getRepository("EvaluatorBundle:Course");
+		$courses = $course_repo->findAll();
+		
+		return $this->render("EvaluatorBundle:Course:index.html.twig", array(
+			"courses" => $courses	
+		));
+	}
+	
+	
 	public function addAction(Request $request) {
 
 		$course = new Course();
@@ -25,20 +36,30 @@ class CourseController extends Controller {
 
 		if ($form->isSubmitted()) {
 			if ($form->isValid()) {
-//			aqui irá la lógica pra dar de alta el nuevo curso
-				$status = "El curso se ha dado de alta correctamente.";
+
+				$em = $this->getDoctrine()->getManager();
+
+				$course = new Course();
+				$course->setName($form->get("name")->getData());
+
+				$em->persist($course);
+				$flush = $em->flush();
+
+				if ($flush == null) {
+					$status = "El curso se ha dado de alta correctamente.";
+				} else {
+					$status = "Se ha producido un error al crear el curso!!";
+				}
 			} else {
 				$status = "El curso no se ha podido dar de alta.";
 			}
 			$this->session->getFlashBag()->add("status", $status);
+			return $this->redirectToRoute("evaluator_index_courses");
 		}
 
 
-
-
-
 		return $this->render("EvaluatorBundle:Course:add.html.twig", array(
-					"form" => $form->createView()
+			"form" => $form->createView()
 		));
 	}
 
