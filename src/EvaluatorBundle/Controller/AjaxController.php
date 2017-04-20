7fb88c3eb42f6bb20431fc95f348f54f622f63f3
kuith <kuith@DESKTOP-17LV1PO>
2017-04-20 21:46:07 +0200

    con javascript_2

---
+++ src/EvaluatorBundle/Controller/AjaxController.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace EvaluatorBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+
+
+/**
+ * Description of AjaxController
+ *
+ * @author kuith
+ */
+class AjaxController extends Controller{
+	
+	public function updatePartialAction(){
+		
+	}
+	
+	public function updateFinalAction(){
+		
+	}
+}
