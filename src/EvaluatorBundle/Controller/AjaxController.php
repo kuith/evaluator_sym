f7ccd61196ed04018682cbc00af4e1af69a27e74
kuith <kuith@DESKTOP-17LV1PO>
2017-04-24 23:22:12 +0200

    con javascript_4

--- src/EvaluatorBundle/Controller/AjaxController.php
+++ src/EvaluatorBundle/Controller/AjaxController.php
@@ -4,6 +4,7 @@ namespace EvaluatorBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use EvaluatorBundle\Entity\Mark;
 
 
 /**
@@ -15,10 +16,21 @@ class AjaxController extends Controller{
 
 	public function updatePartialAction(Request $request){
 		if($request->isXmlHttpRequest()){
-			
+			//$var=$request->query->get("idCourse");
+			//$var=$request->query->get("gradeBefore");
+			$idMark = $request->query->get("idMark");
+			$grade = $request->query->get("grade");
+			var_dump("GET:".$grade);
+
+			$em = $this->getDoctrine()->getManager();
+			$mark = $em->getRepository('evaluatorBundle:Mark')->find($idMark);
+			$mark -> setGrade($grade);
+		    $em->flush();
+			die();
 		}
 	}
 	
+
 	public function updateFinalAction(){
 		
 	}
