422571fc129c46d672dbc67f7852f53a7e63363e
kuith <kuith@DESKTOP-17LV1PO>
2017-05-02 22:47:24 +0200

    con javascript_5

--- src/EvaluatorBundle/Controller/MarkController.php
+++ src/EvaluatorBundle/Controller/MarkController.php
@@ -15,23 +15,4 @@ class MarkController extends Controller {
 	public function __construct() {
 		$this->session = new Session();
 	}
-
-//	public function courseMarksAction($idCourse, $idStudent, $idPartial) {
-//		$em = $this->getDoctrine()->getManager();
-//		$marks_repo = $em->getRepository("EvaluatorBundle:Mark");
-//		$query = $marks_repo->createQueryBuilder('m')
-//				->where('m.idCourse = ?1')
-//				->andWhere('m.idStudent = ?2')
-//				->andWhere('m.idPartial = ?3')
-//				->setParameter(1, $idCourse)
-//				->setParameter(2, $idStudent)
-//				->setParameter(3, $idPartial)
-//				->getQuery();
-//		$mark = $query->setMaxResults(1)->getOneOrNullResult();
-//
-//		return $this->render("EvaluatorBundle:Mark:grid_marks.html.twig", array(
-//					"mark" => $mark
-//		));
-//	}
-
 }
