9b67ae38d290f4529a77435766b3171d71e4ced9
kuith <kuith@DESKTOP-17LV1PO>
2017-05-03 21:54:52 +0200

    con javascript_6

--- src/EvaluatorBundle/Controller/AjaxController.php
+++ src/EvaluatorBundle/Controller/AjaxController.php
@@ -4,7 +4,7 @@ namespace EvaluatorBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use EvaluatorBundle\Entity\Mark;
+use EvaluatorBundle\Controller\MarkController;
 
 
 /**
@@ -16,14 +16,12 @@ class AjaxController extends Controller{
 
 	public function updatePartialAction(Request $request){
 		if($request->isXmlHttpRequest()){
-			//$var=$request->query->get("idCourse");
-			//$var=$request->query->get("gradeBefore");
 			$idMark = $request->query->get("idMark");
 			$grade = $request->query->get("grade");
-			var_dump("GET:".$grade);
+			
 
 			$em = $this->getDoctrine()->getManager();
-			$mark = $em->getRepository('evaluatorBundle:Mark')->find($idMark);
+			$mark = $em->getRepository('EvaluatorBundle:Mark')->findOneBy(array('id' => $idMark));
 			$mark -> setGrade($grade);
 		    $em->flush();
 			die();
--- web/assets/js/custom/markUpdate.js
+++ web/assets/js/custom/markUpdate.js
@@ -5,26 +5,23 @@ $(document).ready(function(){
         markCleaning = function (p_mark){
             return Number(p_mark);
         },
-        cellsblurListener = function (e){
-            var idCourse = e.target.dataset.courseId,
-                idMark = e.target.dataset.markId,
+        cellsBlurListener = function (e){
+            var idMark = e.target.dataset.markId,
                 grade = markCleaning(e.target.innerHTML),
                 grade_before = e.target.dataset.originalMark;
-
 	            if(grade !== grade_before){
 					$.ajax({
-						method:"GET",
-						url: URL + '/evaluator_ajax_updatePartial',
-						data: {'idCourse':idCourse, 'markId':idMark, 'grade':grade, 'gradeBefore':grade_before},
+						type:"POST",
+						url: window.URL+'/ajax/updatePartial',
+						data: {"idMark":idMark, "grade":grade},
 						success: function (response){
 							console.log(response);
 						}
 					});
 				};
-                
             };
-            
-            for (i = 0; i < count; i++) {
-       			cells[i].addEventListener("blur", cellsblurListener, false);
-			}
+
+    for (i = 0; i < count; i++) {
+    	cells[i].addEventListener("blur", cellsBlurListener, false);
+	}
 });
